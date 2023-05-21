b0556a8d0e1379dfad71ffd5b4c37c27626fc991
gramscott <120787031+gramscott@users.noreply.github.com>
2023-05-21 21:00:10 +0100

    MVP completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([
+    { name: "Buy shopping" },
+    { name: "Clean bathroom" },
+    { name: "Car's MOT" },
+  ]); 
+
+  const [todo, setTodo] = useState("")
+
+  const [newTodo, setNewTodo] = useState("")
+
+  const todoNodes = todoList.map((todo, index) => {
+    return (
+      <li key={index}>
+        <span>{todo.name}</span>
+        </li>
+    )
+  })
+
+  const handleTodoInput = (event) => {
+    setNewTodo(event.target.value)
+  }
+
+  const saveNewTodo = (event) => {
+    event.preventDefault();
+    const copyTodoList = [...todoList]
+    copyTodoList.push({name: newTodo})
+    setTodoList(copyTodoList)
+    setNewTodo("")
+  }
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1>ToDo's</h1>
+      <hr/>
+
+      <ul>
+        {todoNodes}
+      </ul>
+
+      <form onSubmit={saveNewTodo}>
+        <label htmlFor="new-todo">Add Todo:</label>
+        <input type="text" id="new-todo" value={newTodo} onChange={handleTodoInput} />
+          <input type="submit" value="Save New Todo"/>
+      </form>
+
+      </div>
+
+);
+  }
 
 export default App;
