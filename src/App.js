a6585998e64cb6b8e1e98610cd3884338cea4a1f
gramscott <120787031+gramscott@users.noreply.github.com>
2023-05-22 14:16:31 +0100

    first part of extensions completed

--- src/App.js
+++ src/App.js
@@ -2,57 +2,108 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-
   const [todoList, setTodoList] = useState([
-    { name: "Buy shopping" },
-    { name: "Clean bathroom" },
-    { name: "Car's MOT" },
-  ]); 
+    { name: "Buy shopping ", priority: "high" },
+    { name: "Clean bathroom ", priority: "high" },
+    { name: "Car's MOT ", priority: "high" },
+    { name: "Watch the rest of Succession ", priority: "low" }
+  ]);
 
-  const [todo, setTodo] = useState("")
+  const [newTodo, setNewTodo] = useState("");
 
-  const [newTodo, setNewTodo] = useState("")
+  const [newTodoPriority, setNewTodoPriority] = useState("low");
 
-  const todoNodes = todoList.map((todo, index) => {
-    return (
-      <li key={index}>
-        <span>{todo.name}</span>
-        </li>
-    )
-  })
+
+const todoNodes = todoList.map((todo, index) => {
+  // console.log("Current todo:", todo);
+  // console.log("Current index:", index);
+
+  return (
+    <li key={index} className={todo.priority === "high" ? "High" : "Low"}>
+      <span>{todo.name}</span>
+      {todo.priority === "high" ? (
+        <span className="High">High</span>
+      ) : (
+        <button onClick={() => prioritizeTodo(index)}>Low</button>
+      )}
+    </li>
+  );
+});
+
+
+const prioritizeTodo = (index) => {
+  // console.log("Index:", index);
+
+  const copyTodoList = [...todoList];
+  // console.log("Copied Todo List:", copyTodoList);
+
+  const updatedTodoList = { ...copyTodoList[index] };
+  // console.log("Updated Todo List Object:", updatedTodoList);
+
+  updatedTodoList.priority = "high";
+  // console.log("Modified Todo List Object:", updatedTodoList);
+
+  copyTodoList[index] = updatedTodoList;
+  // console.log("Modified Copy Todo List:", copyTodoList);
+
+  setTodoList(copyTodoList);
+  // console.log("Updated Todo List:", todoList);
+};
+
 
   const handleTodoInput = (event) => {
-    setNewTodo(event.target.value)
-  }
+    // console.log(event.target.value)
+    setNewTodo(event.target.value);
+  };
+
+  const handlePriorityChange = (event) => {
+    // console.log(event.target.value)
+    setNewTodoPriority(event.target.value);
+  };
 
   const saveNewTodo = (event) => {
     event.preventDefault();
-    const copyTodoList = [...todoList]
-    copyTodoList.push({name: newTodo})
-    setTodoList(copyTodoList)
-    setNewTodo("")
-  }
+    // console.log("Current todo list:", todoList);
+  
+    const copyTodoList = [...todoList];
+    // console.log("Copied todo list:", copyTodoList);
+  
+    const newTodoItem = { name: newTodo, priority: newTodoPriority };
+    // console.log("New todo item:", newTodoItem);
+  
+    copyTodoList.push(newTodoItem);
+    // console.log("Modified todo list:", copyTodoList);
+  
+    setTodoList(copyTodoList);
+    // console.log("Updated todo list:", todoList);
+  
+    setNewTodo("");
+    // console.log("Cleared new todo:", newTodo);
+  };
 
-
+  
   return (
-
     <div className="App">
       <h1>ToDo's</h1>
-      <hr/>
-
-      <ul>
-        {todoNodes}
-      </ul>
-
+      <hr />
+      
       <form onSubmit={saveNewTodo}>
-        <label htmlFor="new-todo">Add Todo:</label>
-        <input type="text" id="new-todo" value={newTodo} onChange={handleTodoInput} />
-          <input type="submit" value="Save New Todo"/>
+        <label htmlFor="new-todo"></label>
+        <input type="text" id="new-todo" value={newTodo} onChange={handleTodoInput} required />
+
+      <div>
+          <input type="radio" id="priority-low" name="priority" value="low" checked={newTodoPriority === "low"} onChange={handlePriorityChange} />
+          <label htmlFor="priority-low">Low</label>
+          <input type="radio" id="priority-high" name="priority" value="high" checked={newTodoPriority === "high"} onChange={handlePriorityChange} />
+          <label htmlFor="priority-high">High</label>
+        </div>
+
+        <input type="submit" value="Save Item" />
       </form>
 
-      </div>
-
-);
-  }
+      <ol>{todoNodes}</ol>
+    </div>
+  );
+}
 
 export default App;
